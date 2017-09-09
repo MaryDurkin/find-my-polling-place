8445f1c91c70977c88114861534bc4cb590a0527
Mary <mary.walsh@gmail.com>
2017-09-09 11:31:42 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Find My Polling Place
+
+A Google Maps based app for locating polling places in Lower Merion Township, PA
+
+### Created by Mary Walsh
+
+Disclaimer: This app is based on ward and precinct maps provided by Lower Merion Township and Montgomery County Voter Services. If you are on close to a precinct border double check with voter services http://www.montcopa.org/
---
+++ js/app.js
@@ -0,0 +1,61 @@
+/* draws overlays of teh precints using points defined in coords.js */
+var map;
+var infowindow;
+
+
+
+function initMap() {
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.9985, lng: -75.289},
+    zoom: 15
+  });
+  infoWindow = new google.maps.InfoWindow({});
+
+  //construct the polygons
+    var len = precincts.length;
+    var polygon;
+    var currentPrecinict;
+    var lastPrecinct;
+    for (var i=0; i < len; i++){
+          polygon = new google.maps.Polygon({
+          paths: precincts[i].coords,
+          strokeColor: precincts[i].strokeColor,
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: precincts[i].fillColor,
+          fillOpacity: 0.15
+        });
+        polygon.setMap(map);
+
+        precincts[i].polygon = polygon;
+
+        polygon.addListener('click', (function(polygonCopy, iCopy) {
+          return function(){
+            if (lastPrecinct) {
+            lastPrecinct.polygon.setMap(null);
+            lastPrecinct.polygon.fillOpacity = 0.15;
+            lastPrecinct.polygon.setMap(map);
+            }
+            currentPrecinct = precincts[iCopy];
+            currentPrecinct.polygon.setMap(null);
+            currentPrecinct.polygon.fillOpacity = 0.3;
+            currentPrecinct.polygon.setMap(map);
+            lastPrecinct = currentPrecinct;
+            showPollPlace(currentPrecinct);
+          };
+
+        })(polygon, i));
+
+    }
+
+    function showPollPlace(precinct) {
+
+      // Replace the info window's content and position.
+      infoWindow.setContent(precinct.contentString);
+      infoWindow.setPosition(precinct.position);
+
+      infoWindow.open(map);
+    }
+
+}
